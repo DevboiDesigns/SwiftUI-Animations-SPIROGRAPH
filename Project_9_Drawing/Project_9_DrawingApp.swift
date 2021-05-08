bd11cc8981c5bdbe7f521b042e7a0799ac34e7ea
Christopher <visrule@protonmail.com>
2021-05-08 06:44:20 -0500

    Initial Commit

---
+++ Project_9_Drawing/Project_9_DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_9_DrawingApp.swift
+//  Project_9_Drawing
+//
+//  Created by Christopher Hicks on 5/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Project_9_DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
